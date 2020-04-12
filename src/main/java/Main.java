dd0685fc8bfd7f83544a6516bc68e766e6ed41b6
Oscar De Alva <oscar.ivan98@gmail.com>
2020-04-11 19:19:36 -0500

    Final version of RC4 modification

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,23 @@
 import java.nio.charset.StandardCharsets;
-import RC4.RC4;
+import RC4.mod.RC4Mod;
 
 public class Main {
-    public static void main(String... strings) {
-        String text = "Oscar";
-        String key = "dulio";
-        RC4 rc = new RC4(key.getBytes(StandardCharsets.UTF_8));
+    public static void main(String... args) {
+        if(args.length < 3) {
+            System.out.println("Program must be called with 3 arguments:");
+            System.out.println("[message] [key 1] [key 2]");
+            return;
+        }
+
+        String text = args[0];
+        String key1 = args[1];
+        String key2 = args[2];
+        RC4Mod rc = new RC4Mod(key1.getBytes(StandardCharsets.UTF_8), key2.getBytes(StandardCharsets.UTF_8));
 
         System.out.println("Original text:");
         System.out.println(text);
+        System.out.println("Keys:");
+        System.out.println("1: " + key1 + "\t" + "2: " + key2);
 
         System.out.println("Encryption:");
         byte[] enc = rc.encrypt(text.getBytes(StandardCharsets.UTF_8));
--- src/main/java/RC4/mod/RC4Mod.java
+++ src/main/java/RC4/mod/RC4Mod.java
@@ -3,19 +3,19 @@ package RC4.mod;
 public class RC4Mod {
     private final int N = 256;
     private final int half = N / 2;
-    private final byte[] S1 = new byte[half];
-    private final byte[] S2 = new byte[half];
+    private final int[] S1 = new int[half];
+    private final int[] S2 = new int[half];
     private final byte[] k1;
     private final byte[] k2;
 
-    private void swap(byte[] arr, int i, int j) {
-        byte temp = arr[j];
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[j];
         arr[j] = arr[i];
         arr[i] = temp;
     }
 
-    private void swap(byte[] a, byte[] b, int i, int j) {
-        byte temp = b[j];
+    private void swap(int[] a, int[] b, int i, int j) {
+        int temp = b[j];
         b[j] = a[i];
         a[i] = temp;
     }
@@ -24,20 +24,20 @@ public class RC4Mod {
         // Initializing both states
         for (int i = 0; i < half - 1; i++) {
             S1[i] = i;
+            // S2[i] = (byte) i;
         }
+
         for (int i = half; i < N - 1; i++) {
             S2[i - half] = i;
         }
 
         int j = 0;
         for (int i = 0; i < half - 1; i++) {
-            j = j + S1[i] + k1[i % k1.length] % half;
+            j = (j + S1[i] + k1[i % k1.length]) % half;
             swap(S1, i, j);
-
-            j = j + S2[i] + k2[i % k2.length] % half;
+            j = (j + S2[i] + k2[i % k2.length]) % half;
             swap(S2, i, j);
         }
-
     }
 
     public RC4Mod(final byte[] key1, final byte[] key2) {
@@ -53,7 +53,6 @@ public class RC4Mod {
         KSA();
         final byte[] ciphertext = new byte[plaintext.length];
         int i = 0, j1 = 0, j2 = 0, t1, t2;
-        byte temp;
         for (int counter = 0; counter < plaintext.length; counter += 2) {
             i = (i + 1) & 0x7F;
             j1 = (j1 + S1[i]) & 0x7F;
@@ -61,21 +60,21 @@ public class RC4Mod {
             // Swap S1[i] with S2[j1]
             swap(S1, S2, i, j1);
 
-            t1 = S1[(S1[i] + S1[j1]) & 0x7F];
+            t1 = (S1[(S1[i] + S1[j1]) & 0x7F]) & 0x7F;
             j2 = (j2 + S2[i]) & 0x7F;
 
             // Swap S2[i] with S1[j2]
             swap(S2, S1, i, j2);
-
-            t2 = S2[(S2[i] + S2[j2]) & 0x7F];
+            t2 = (S2[(S2[i] + S2[j2]) & 0x7F]) & 0x7F;
 
             // Swap S1[t1] with S2[t2]
             swap(S1, S2, t1, t2);
 
             // XOR with plaintext and add to ciphertext
             ciphertext[counter] = (byte) (plaintext[counter] ^ t1);
-            ciphertext[counter + 1] = (byte) (plaintext[counter + 1] ^ t2);
-
+            if (counter + 1 < plaintext.length) {
+                ciphertext[counter + 1] = (byte) (plaintext[counter + 1] ^ t2);
+            }
         }
         return ciphertext;
     }
