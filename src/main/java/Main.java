d88c7e7236e20e4f3df63026bac2e22bb1910fda
Oscar De Alva <oscar.ivan98@gmail.com>
2020-04-16 11:02:28 -0500

    Added option for encryption/decryption using original RC4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,19 @@
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.LongSummaryStatistics;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 import com.google.common.collect.Streams;
 
-import RC4.RC4;
-import RC4.mod.RC4Mod;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.commons.lang3.tuple.Pair;
+
+import RC4.RC4;
+import RC4.mod.RC4Mod;
 
 public class Main {
 
@@ -23,13 +22,13 @@ public class Main {
     }
 
     private static String getAvgTime(String plaintext, int epochs, Function<byte[], Long> algorithm) {
-        LongSummaryStatistics times = LongStream.range(0, epochs)
-                .map(i -> algorithm.apply(toBytes(plaintext))).summaryStatistics();
+        LongSummaryStatistics times = LongStream.range(0, epochs).map(i -> algorithm.apply(toBytes(plaintext)))
+                .summaryStatistics();
         return String.format("%.4f", times.getAverage());
     }
 
     public static void main(String... args) {
-        if (args.length > 0 && args.length < 3 && args[0].equals("test")) {
+        if (args.length > 0 && args.length < 3 && args[0].equals(":test")) {
             int numberOfEpochs = 20;
             if (args.length == 2) {
                 numberOfEpochs = NumberUtils.toInt(args[1], numberOfEpochs);
@@ -47,8 +46,8 @@ public class Main {
             System.out.println("Epochs: " + epochs);
             List<String> plaintexts = sizes.stream().map(time -> StringUtils.repeat(str, time))
                     .collect(Collectors.toList());
-            List<String> encryptionsRC4 = plaintexts.stream()
-                    .map(pt -> new String(original.encrypt(toBytes(pt)))).collect(Collectors.toList());
+            List<String> encryptionsRC4 = plaintexts.stream().map(pt -> new String(original.encrypt(toBytes(pt))))
+                    .collect(Collectors.toList());
             List<String> encryptionsModRC4 = plaintexts.stream().map(pt -> new String(mod.encrypt(toBytes(pt))))
                     .collect(Collectors.toList());
 
@@ -70,10 +69,30 @@ public class Main {
             System.out.println("Average times for RC4 Modification Decryptions:");
             Streams.zip(sizes.stream(), timedDecryptionsModRC4.stream(), Pair::of).forEach(System.out::println);
 
-        } else if (args.length < 3) {
-            System.out.println("Program must be called with 3 arguments:");
-            System.out.println("[message] [key 1] [key 2]");
-            System.exit(1);
+        } else if (args.length == 2) {
+            String text = args[0];
+            String key = args[1];
+            RC4 rc = new RC4(toBytes(key));
+
+            System.out.println("Original text:");
+            System.out.println(text);
+            System.out.println("Key:");
+            System.out.println(key);
+
+            System.out.println("Encryption:");
+            byte[] enc = rc.encrypt(toBytes(text));
+            System.out.println(new String(enc));
+
+            System.out.println("Decryption");
+            byte[] dec = rc.encrypt(enc);
+            System.out.println(new String(dec));
+        } else if (args.length > 3 || args.length == 0) {
+            System.out.println("Invalid arguments for the program");
+            System.out.println("Program can be called these ways:");
+            System.out.println("[message] [key 1] [key 2] \t- To encrypt/decrypt with Modified RC4");
+            System.out.println("[message] [key] \t\t- To encrypt/decrypt with Original RC4");
+            System.out.println(":test [number of iterations] \t- To test implementations");
+            System.exit(0);
         } else {
             String text = args[0];
             String key1 = args[1];
@@ -93,6 +112,5 @@ public class Main {
             byte[] dec = rc.encrypt(enc);
             System.out.println(new String(dec));
         }
-
     }
 }
